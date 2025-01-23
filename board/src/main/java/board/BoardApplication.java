f2d4ea916c4d8422810526f2954b8875fb43a06b
myanjini <myanjini@gmail.com>
2025-01-23 13:40:23 +0900

    Log4JDBC 추가

--- board/src/main/java/board/BoardApplication.java
+++ board/src/main/java/board/BoardApplication.java
@@ -4,19 +4,10 @@ package board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @SpringBootApplication
 public class BoardApplication {
     
 	public static void main(String[] args) {
-	    log.trace("trace");
-	    log.debug("debug");
-	    log.info("info");
-	    log.warn("warn");
-	    log.error("error");
-	    
 	    SpringApplication.run(BoardApplication.class, args);
 	}
 
