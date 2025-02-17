dc504fc828edbba3b968c032d2330eb068160a58
myanjini <myanjini@gmail.com>
2025-02-18 07:37:24 +0900

    컨피그 서버, 라이센스 서버 구현 및 연동

---
+++ license-service/src/main/java/com/optimagrowth/license/repository/LicenseRepository.java
@@ -0,0 +1,12 @@
+package com.optimagrowth.license.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.optimagrowth.license.model.License;
+
+public interface LicenseRepository extends CrudRepository<License, String> {
+    public List<License> findByOrganizationId(String organizationId);
+    public License findByOrganizationIdAndLicenseId(String organizationId, String licenseId);
+}
---
+++ license-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -0,0 +1,58 @@
+package com.optimagrowth.license.service;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+import com.optimagrowth.license.config.ServiceConf;
+import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.repository.LicenseRepository;
+
+@Service
+public class LicenseService {
+    @Autowired
+    MessageSource messages;
+    
+    @Autowired
+    LicenseRepository licenseRepository;
+    
+    @Autowired
+    ServiceConf config;
+    
+    public License getLicense(String licenseId, String organizationId) {
+        License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
+        if (license == null) {
+            throw new IllegalArgumentException(
+                String.format(
+                    messages.getMessage("license.search.error.message", null, null), 
+                    licenseId, organizationId));
+            
+        }        
+        return license.withComment(config.getProperty());
+    }
+    
+    public License createLicense(License license) {
+        license.setLicenseId(UUID.randomUUID().toString());
+        licenseRepository.save(license);
+        return license.withComment(config.getProperty());
+    }
+    
+    public License updateLicense(License license) {
+        licenseRepository.save(license);
+        return license.withComment(config.getProperty());
+    }
+    
+    public String deleteLicense(String licenseId) {
+        License license = new License();
+        license.setLicenseId(licenseId);
+        licenseRepository.delete(license);
+        
+        String responseMessage = null;
+        responseMessage 
+            = String.format(messages.getMessage("license.delete.message", null, null), 
+                licenseId);
+        return responseMessage;
+    }
+}
